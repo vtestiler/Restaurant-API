50cda09d1e5a1b741bf3ffa52c7bb5d54f11cb08
vladimir testiler <vtestiler@yahoo.com>
2019-03-25 09:07:22 -0400

    modified db.js connection string to get rid of Deprecation Warning message.

--- src/db.js
+++ src/db.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 import config from './config';
 
 export default callback => {
-  let db = mongoose.connect(config.mongoUrl);
+  let db = mongoose.connect(config.mongoUrl, {useNewUrlParser: true});
   callback(db);
 }
